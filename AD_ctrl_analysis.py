76d391e8be1c65689c9a8b15067ab1ddc0dde886
Qinghao <qwen2355@uni.sydney.edu.au>
2025-12-15 11:53:57 +1100

    added tau protein labelling

--- AD_ctrl_analysis.py
+++ AD_ctrl_analysis.py
@@ -21,20 +21,20 @@ npx_matrix = samples_long.pivot(
 npx_matrix = npx_matrix.sort_index(axis=0)
 npx_matrix = npx_matrix.sort_index(axis=1)
 
-print(npx_matrix.shape)
+# print(npx_matrix.shape)
 
-plt.figure(figsize=(24, 6))
-ax = sns.heatmap(
-    npx_matrix,
-    cmap="viridis",
-    cbar_kws={"label": "NPX"},
-    xticklabels=False
-)
-ax.set_xlabel("Assays")
-ax.set_ylabel("Samples")
-ax.set_title("NPX heatmap")
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(24, 6))
+# ax = sns.heatmap(
+#     npx_matrix,
+#     cmap="viridis",
+#     cbar_kws={"label": "NPX"},
+#     xticklabels=False
+# )
+# ax.set_xlabel("Assays")
+# ax.set_ylabel("Samples")
+# ax.set_title("NPX heatmap")
+# plt.tight_layout()
+# plt.show()
 
 npx = npx_matrix
 
@@ -82,126 +82,190 @@ dep_flags = pd.DataFrame(
 dep_flags["n_comparisons"] = dep_flags.sum(axis=1)
 # dep_flags.to_csv("DEPs_overlap_fc_only_flags.csv")
 # Fix the order of comparisons (rows)
-comparison_order = list(comparisons_fc_only.keys())
+# comparison_order = list(comparisons_fc_only.keys())
 
-# Fix the order of assays (columns) – should be 1034
-assay_order = npx.columns.to_list()
-print(assay_order)
-# Stack fold-change vectors row-wise
-fc_matrix = np.vstack([
-    dep_results_fc[name]
-        .set_index("Assay")
-        .loc[assay_order, "FoldChange"]
-        .values
-    for name in comparison_order
-])
+# # Fix the order of assays (columns) – should be 1034
+# assay_order = npx.columns.to_list()
+# print(assay_order)
+# # Stack fold-change vectors row-wise
+# fc_matrix = np.vstack([
+#     dep_results_fc[name]
+#         .set_index("Assay")
+#         .loc[assay_order, "FoldChange"]
+#         .values
+#     for name in comparison_order
+# ])
 
-print("Fold-change matrix shape:", fc_matrix.shape)  # should be (5, 1034)
+# print("Fold-change matrix shape:", fc_matrix.shape)  # should be (5, 1034)
 
-# Optional: log2 transform so colours are symmetric around 0
-log2_fc_matrix = np.log2(fc_matrix)
+# # Optional: log2 transform so colours are symmetric around 0
+# log2_fc_matrix = np.log2(fc_matrix)
 
-plt.figure(figsize=(24, 4))
-ax = sns.heatmap(
-    log2_fc_matrix,
-    cmap="bwr",          # red/blue centred at 0
-    center=0,            # log2(FC) = 0  => FC = 1 (no change)
-    cbar_kws={"label": "log2(Fold change AD/Ctrl)"},
-    xticklabels=False,
-    yticklabels=comparison_order
+# plt.figure(figsize=(24, 4))
+# ax = sns.heatmap(
+#     log2_fc_matrix,
+#     cmap="bwr",          # red/blue centred at 0
+#     center=0,            # log2(FC) = 0  => FC = 1 (no change)
+#     cbar_kws={"label": "log2(Fold change AD/Ctrl)"},
+#     xticklabels=False,
+#     yticklabels=comparison_order
+# )
+# ax.set_xlabel("Assays")
+# ax.set_ylabel("Comparisons")
+# ax.set_title("Fold change heatmap (5 x 1034)")
+# plt.tight_layout()
+# plt.show()
+
+
+
+# comparisons = {
+#     "Ctrl_vs_L": (
+#         ["Ctrl-S_1pg", "Ctrl-S_5pg", "Ctrl-S_10pg", "Ctrl-S_20pg", "Ctrl-S_40pg"],
+#         ["L-1", "L-2"]
+#     )
+# }
+
+# ctrl_group, lys_group = comparisons["Ctrl_vs_L"]
+
+# fold_changes = []
+# p_values = []
+# assays = []
+
+# for assay in npx.columns:
+#     ctrl_vals = npx.loc[ctrl_group, assay].dropna()
+#     lys_vals  = npx.loc[lys_group, assay].dropna()
+#     if len(ctrl_vals) == 0 or len(lys_vals) == 0:
+#         continue
+#     delta_npx = ctrl_vals.mean() - lys_vals.mean()
+#     fc = 2 ** delta_npx
+#     fold_changes.append(fc)
+#     assays.append(assay)
+#     stat, p = ranksums(ctrl_vals, lys_vals)
+#     p_values.append(p)
+
+# fold_changes = np.array(fold_changes)
+# p_values = np.array(p_values)
+
+# deps_mask = ((fold_changes >= fc_threshold) | (fold_changes <= 1 / fc_threshold)) & (p_values <= 1)
+# n_deps = deps_mask.sum()
+
+# print("Ctrl-S_vs_L =", n_deps)
+
+# deps_df = pd.DataFrame({
+#     "Assay": np.array(assays),
+#     "FoldChange": fold_changes,
+#     "p_value": p_values,
+#     "DEP": deps_mask
+# })
+
+# deps_only = deps_df[deps_df["DEP"]].sort_values("p_value")
+# print(deps_only.head())
+
+# pooled_comparisons = {
+#     "Pooled_AD_vs_Pooled_Ctrl": (
+#         ["AD-S_1pg", "AD-S_5pg", "AD-S_10pg", "AD_20pg", "AD-S_40pg"],
+#         ["Ctrl-S_1pg", "Ctrl-S_5pg", "Ctrl-S_10pg", "Ctrl-S_20pg", "Ctrl-S_40pg"]
+#     )
+# }
+
+# ad_group_pooled, ctrl_group_pooled = pooled_comparisons["Pooled_AD_vs_Pooled_Ctrl"]
+
+# fold_changes_pooled = []
+# p_values_pooled = []
+# assays_pooled = []
+
+# for assay in npx.columns:
+#     ad_vals = npx.loc[ad_group_pooled, assay].dropna()
+#     ctrl_vals = npx.loc[ctrl_group_pooled, assay].dropna()
+#     if len(ad_vals) == 0 or len(ctrl_vals) == 0:
+#         continue
+#     delta_npx = ad_vals.mean() - ctrl_vals.mean()
+#     fc = 2 ** delta_npx
+#     fold_changes_pooled.append(fc)
+#     assays_pooled.append(assay)
+#     stat, p = ranksums(ad_vals, ctrl_vals)
+#     p_values_pooled.append(p)
+
+# fold_changes_pooled = np.array(fold_changes_pooled)
+# p_values_pooled = np.array(p_values_pooled)
+
+# deps_mask_pooled = ((fold_changes_pooled >= fc_threshold) | (fold_changes_pooled <= 1 / fc_threshold)) & (p_values_pooled <= 1)
+# n_deps_pooled = deps_mask_pooled.sum()
+
+# print("Pooled_AD_vs_Pooled_Ctrl =", n_deps_pooled)
+
+# deps_df_pooled = pd.DataFrame({
+#     "Assay": np.array(assays_pooled),
+#     "FoldChange": fold_changes_pooled,
+#     "p_value": p_values_pooled,
+#     "DEP": deps_mask_pooled
+# })
+
+# deps_only_pooled = deps_df_pooled[deps_df_pooled["DEP"]].sort_values("p_value")
+# print(deps_only_pooled.head())
+
+
+# load in tau associated proteins
+tau_file = r"C:\Users\qingh\OneDrive - The University of Sydney (Students)\VRIproteomics\Tau_proteins_analysis_merged.csv"
+
+data_tau = pd.read_csv(tau_file)
+
+# mask_samples = data_AD["SampleType"] == "SAMPLE"
+# mask_assays  = data_AD["AssayType"] == "assay"
+
+# samples_long = data_AD.loc[mask_samples & mask_assays].copy()
+
+# npx_matrix = samples_long.pivot(
+#     index="SampleID",
+#     columns="Assay",
+#     values="NPX"
+# )
+
+# npx_matrix = npx_matrix.sort_index(axis=0)
+# npx_matrix = npx_matrix.sort_index(axis=1)
+
+tau_proteins = list(data_tau["Protein"])
+# print(tau_proteins)
+
+dep_results_fc = {}
+comparisons_fc_only = {
+    "AD-S_1pg_vs_Ctrl-S_1pg": ("AD-S_1pg", "Ctrl-S_1pg"),
+    "AD-S_5pg_vs_Ctrl-S_5pg": ("AD-S_5pg", "Ctrl-S_5pg"),
+    "AD-S_10pg_vs_Ctrl-S_10pg": ("AD-S_10pg", "Ctrl-S_10pg"),
+    "AD_20pg_vs_Ctrl-S_20pg": ("AD_20pg", "Ctrl-S_20pg"),
+    "AD-S_40pg_vs_Ctrl-S_40pg": ("AD-S_40pg", "Ctrl-S_40pg")
+}
+
+for name, (ad_group, ctrl_group) in comparisons_fc_only.items():
+    fold_changes = []
+    for assay in npx.columns:
+        ad_vals = npx.loc[ad_group, assay]
+        ctrl_vals = npx.loc[ctrl_group, assay]
+        delta_npx = ad_vals - ctrl_vals
+        fc = 2 ** delta_npx
+        fold_changes.append(fc)
+    fold_changes = np.array(fold_changes)
+    deps_mask = (fold_changes >= fc_threshold) | (fold_changes <= 1 / fc_threshold)
+    n_deps = deps_mask.sum()
+    print(name, "=", n_deps)
+    
+    deps_df = pd.DataFrame({
+        "Assay": np.array(npx.columns),
+        "FoldChange": fold_changes,
+        "DEP": deps_mask
+    })
+
+    deps_only = deps_df[deps_df["DEP"]].sort_values("FoldChange")
+    dep_results_fc[name] = deps_df
+
+dep_flags = pd.DataFrame(
+    {
+        name: df.set_index("Assay")["DEP"]
+        for name, df in dep_results_fc.items()
+    }
 )
-ax.set_xlabel("Assays")
-ax.set_ylabel("Comparisons")
-ax.set_title("Fold change heatmap (5 x 1034)")
-plt.tight_layout()
-plt.show()
 
-
-
-comparisons = {
-    "Ctrl_vs_L": (
-        ["Ctrl-S_1pg", "Ctrl-S_5pg", "Ctrl-S_10pg", "Ctrl-S_20pg", "Ctrl-S_40pg"],
-        ["L-1", "L-2"]
-    )
-}
-
-ctrl_group, lys_group = comparisons["Ctrl_vs_L"]
-
-fold_changes = []
-p_values = []
-assays = []
-
-for assay in npx.columns:
-    ctrl_vals = npx.loc[ctrl_group, assay].dropna()
-    lys_vals  = npx.loc[lys_group, assay].dropna()
-    if len(ctrl_vals) == 0 or len(lys_vals) == 0:
-        continue
-    delta_npx = ctrl_vals.mean() - lys_vals.mean()
-    fc = 2 ** delta_npx
-    fold_changes.append(fc)
-    assays.append(assay)
-    stat, p = ranksums(ctrl_vals, lys_vals)
-    p_values.append(p)
-
-fold_changes = np.array(fold_changes)
-p_values = np.array(p_values)
-
-deps_mask = ((fold_changes >= fc_threshold) | (fold_changes <= 1 / fc_threshold)) & (p_values <= 1)
-n_deps = deps_mask.sum()
-
-print("Ctrl-S_vs_L =", n_deps)
-
-deps_df = pd.DataFrame({
-    "Assay": np.array(assays),
-    "FoldChange": fold_changes,
-    "p_value": p_values,
-    "DEP": deps_mask
-})
-
-deps_only = deps_df[deps_df["DEP"]].sort_values("p_value")
-print(deps_only.head())
-
-pooled_comparisons = {
-    "Pooled_AD_vs_Pooled_Ctrl": (
-        ["AD-S_1pg", "AD-S_5pg", "AD-S_10pg", "AD_20pg", "AD-S_40pg"],
-        ["Ctrl-S_1pg", "Ctrl-S_5pg", "Ctrl-S_10pg", "Ctrl-S_20pg", "Ctrl-S_40pg"]
-    )
-}
-
-ad_group_pooled, ctrl_group_pooled = pooled_comparisons["Pooled_AD_vs_Pooled_Ctrl"]
-
-fold_changes_pooled = []
-p_values_pooled = []
-assays_pooled = []
-
-for assay in npx.columns:
-    ad_vals = npx.loc[ad_group_pooled, assay].dropna()
-    ctrl_vals = npx.loc[ctrl_group_pooled, assay].dropna()
-    if len(ad_vals) == 0 or len(ctrl_vals) == 0:
-        continue
-    delta_npx = ad_vals.mean() - ctrl_vals.mean()
-    fc = 2 ** delta_npx
-    fold_changes_pooled.append(fc)
-    assays_pooled.append(assay)
-    stat, p = ranksums(ad_vals, ctrl_vals)
-    p_values_pooled.append(p)
-
-fold_changes_pooled = np.array(fold_changes_pooled)
-p_values_pooled = np.array(p_values_pooled)
-
-deps_mask_pooled = ((fold_changes_pooled >= fc_threshold) | (fold_changes_pooled <= 1 / fc_threshold)) & (p_values_pooled <= 1)
-n_deps_pooled = deps_mask_pooled.sum()
-
-print("Pooled_AD_vs_Pooled_Ctrl =", n_deps_pooled)
-
-deps_df_pooled = pd.DataFrame({
-    "Assay": np.array(assays_pooled),
-    "FoldChange": fold_changes_pooled,
-    "p_value": p_values_pooled,
-    "DEP": deps_mask_pooled
-})
-
-deps_only_pooled = deps_df_pooled[deps_df_pooled["DEP"]].sort_values("p_value")
-print(deps_only_pooled.head())
-
-
+dep_flags["n_comparisons"] = dep_flags.sum(axis=1)
+dep_flags["tau protein"] = list(i in tau_proteins for i in dep_flags.index)
+dep_flags.to_csv("DEPs_overlap_fc_only_flags_tauproteins_labelled.csv")
+# print(list(i in tau_proteins for i in dep_flags.index))
